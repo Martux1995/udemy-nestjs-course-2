b31e102c7d07e8bfa51c15abf859f5e6ca80aab3
Martux1995 <bastianruizgaray@gmail.com>
2022-11-29 02:08:26 -0300

    :bug: Fixed dto validation

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -4,12 +4,11 @@ import { IsOptional, IsPositive, Min } from 'class-validator';
 export class PaginationDto {
   @IsOptional()
   @IsPositive()
-  @Min(1)
   @Type(() => Number)
   limit?: number;
 
   @IsOptional()
-  @IsPositive()
+  @Min(0)
   @Type(() => Number)
   offset?: number;
 }
